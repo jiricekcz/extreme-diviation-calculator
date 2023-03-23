375b02f6b09d47807d4a7bb1ef1b877a412f98a2
Jiří Cihelka <36630605+jiricekcz@users.noreply.github.com>
2023-03-23 10:55:15 +0100

    s

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,1 +1,8 @@
+interface Value {
+    value: number;
+    minDiff: number;
+    maxDiff: number;
+}
+declare function calculateValue<T extends Array<Value>>(func: (values: number[]) => number, values: T, maxOps?: number): Value;
+declare function valueAt(value: Value, index: number, loopSize: number): number;
 //# sourceMappingURL=index.d.ts.map
--- build/index.js
+++ build/index.js
@@ -1,3 +1,32 @@
 "use strict";
-console.log("Hello world!");
+function calculateValue(func, values, maxOps = 1000000) {
+    const loopSize = Math.pow(maxOps, 1 / values.length);
+    let min = Number.POSITIVE_INFINITY;
+    let max = Number.NEGATIVE_INFINITY;
+    const [value, ...valuesNext] = [...values];
+    if (valuesNext.length === 0)
+        return value;
+    for (let i = 0; i < loopSize; i++) {
+        const v = calculateValue((values) => {
+            return func([valueAt(value, i, loopSize), ...values]);
+        }, valuesNext, maxOps - loopSize);
+        if (v.value < min)
+            min = v.value;
+        if (v.value > max)
+            max = v.value;
+    }
+    const v = func(values.map(value => value.value));
+    return {
+        value: v,
+        minDiff: min - v,
+        maxDiff: max - v
+    };
+}
+function valueAt(value, index, loopSize) {
+    return value.value - value.minDiff + (value.minDiff + value.maxDiff) * index / Math.floor(loopSize);
+}
+console.log(calculateValue(([a, b]) => a + b, [
+    { value: 46.4, minDiff: -0.2, maxDiff: 0.3 },
+    { value: 72.1, minDiff: -0.3, maxDiff: 0.3 }
+]));
 //# sourceMappingURL=index.js.map
